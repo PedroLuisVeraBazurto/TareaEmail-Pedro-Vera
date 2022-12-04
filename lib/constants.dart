4d4385221ba7197345eb0553b1b5b928f223726e
PedroLuisVeraBazurto <92538964+PedroLuisVeraBazurto@users.noreply.github.com>
2022-12-04 00:38:54 -0500

    Proyecto

---
+++ lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.green;
+
+const fromTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w700,
+);
+
+const dateTextStyle = TextStyle(
+  color: Colors.black45,
+  fontSize: 15.0,
+  fontWeight: FontWeight.w300,
+);
+
+const subjectTextStyle = TextStyle(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w400,
+);
+
+const bodyTextStyle = TextStyle(
+  fontSize: 16.0,
+);
